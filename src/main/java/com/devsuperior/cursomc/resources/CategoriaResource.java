65b0455904c46b05b01a6b6f1b6679813f519426
Wenderson Santos <guto15santos@gmail.com>
2022-02-14 08:32:11 -0300

    Testando classe de domínio

---
+++ src/main/java/com/devsuperior/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,31 @@
+package com.devsuperior.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method =RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
+		
+	}
+
+}
