88825c6af6b22d5cf9350e9307b1edb36664e93b
Wenderson Santos <guto15santos@gmail.com>
2022-02-17 16:03:10 -0300

    Tratamento para referencia ciclica

--- src/main/java/com/devsuperior/cursomc/domain/Produto.java
+++ src/main/java/com/devsuperior/cursomc/domain/Produto.java
@@ -15,6 +15,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -25,6 +27,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 	
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "produto_categoria",
 	joinColumns = @JoinColumn(name = "produto_id"),
