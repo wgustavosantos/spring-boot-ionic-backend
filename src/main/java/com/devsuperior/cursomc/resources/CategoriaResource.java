0be4f4c46e083f5171f9c40ef0d637ecd3a42353
Wenderson Santos <guto15santos@gmail.com>
2022-02-25 23:43:58 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/devsuperior/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/devsuperior/cursomc/resources/CategoriaResource.java
@@ -1,10 +1,11 @@
 package com.devsuperior.cursomc.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -53,33 +55,43 @@ public class CategoriaResource {
 	
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Void> udpdate(@PathVariable Integer id, @RequestBody Categoria obj ){
-		
 		obj.setId(id);
 		obj = categoriaService.update(obj);
 		
 		return ResponseEntity.noContent().build();
-		
 	}
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
-		
 		categoriaService.delete(id);
 		
 		return ResponseEntity.noContent().build();
-		
 	}
 	
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> listObj = categoriaService.findAll();
-		
-		List<CategoriaDTO> listObjDTO = new ArrayList<>();
-		
-		for (Categoria categoria : listObj) {
+		List<CategoriaDTO> listObjDTO = 
+				listObj.stream().map
+				(obj -> new CategoriaDTO(obj))
+				.collect(Collectors.toList());
+		/*for (Categoria categoria : listObj) {
 			listObjDTO.add(new CategoriaDTO(categoria));
-		}
-		
+		}*/
+		return ResponseEntity.ok().body(listObjDTO);
+	}
+	
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24")  Integer linesPerPage,/*24 pois é multiplo de 1, 2, 3 e 4 para criar layouts sem quebrar */
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC")  String direction) {
+		Page<Categoria> listObj = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listObjDTO = 
+				listObj.map
+				(obj -> new CategoriaDTO(obj));			
+	
 		return ResponseEntity.ok().body(listObjDTO);
 	}
 
--- src/main/java/com/devsuperior/cursomc/services/CategoriaService.java
+++ src/main/java/com/devsuperior/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.cursomc.domain.Categoria;
@@ -47,10 +50,18 @@ public class CategoriaService {
 		}
 
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
-		
+
 	}
-		
+
+	public Page<Categoria> findPage(Integer page, 
+			Integer linesPerPage, 
+			String orderBy, 
+			String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
+	}
+
 }
