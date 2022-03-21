f12d6bccc195d0d77f95565c7e2e24b5498e7a15
Wenderson Santos <guto15santos@gmail.com>
2022-03-20 22:32:50 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/devsuperior/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/devsuperior/cursomc/repositories/ClienteRepository.java
@@ -1,11 +1,20 @@
 package com.devsuperior.cursomc.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	/* Adicionar o prefixo findBy + o nome do atributo da entidade que quer buscar no banco de dados */
+	/* O parâmetro “readOnly” na anotação @Transactional epecifica que 
+	 * nenhuma operação de DML poderá ser executada (Insert, Delete ou Update), ou seja, apenas consultas.
+	 */
+	@Transactional(readOnly = true) /* org.springframework.transaction.annotation.Transactional */
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/devsuperior/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/devsuperior/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,15 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.devsuperior.cursomc.domain.Cliente;
 import com.devsuperior.cursomc.domain.dtos.ClienteNewDTO;
 import com.devsuperior.cursomc.domain.enums.TipoCliente;
+import com.devsuperior.cursomc.repositories.ClienteRepository;
 import com.devsuperior.cursomc.resources.exception.FieldMessage;
 import com.devsuperior.cursomc.services.validation.utils.BR;
 
 //nome do validator
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> { // nome da anotação,
-																									// para qual classe
-																									// se destina
+	
+	@Autowired																						// para qual classe
+	ClienteRepository repo;																			// se destina
 	@Override
 	public void initialize(ClienteInsert ann) {// nome da anotação também
 	}
@@ -32,8 +37,14 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 
-			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("Email", "Email já cadastrado"));
+		}
+		
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
