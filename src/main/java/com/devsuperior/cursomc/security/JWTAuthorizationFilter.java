0c398d7ca24f46949a37f6f2435618dff0eb8b6c
Wenderson Santos <guto15santos@gmail.com>
2022-05-09 21:39:48 -0300

    Filtro de autorização JWT criado

---
+++ src/main/java/com/devsuperior/cursomc/security/JWTAuthorizationFilter.java
@@ -0,0 +1,55 @@
+package com.devsuperior.cursomc.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+/*Filtro de autorização de token \ Busca um usuario na base e verificia se o usuario do token existe*/
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+	private JWTUtil jwtUtil;
+	private UserDetailsService userDetailsService; /* */
+
+	public JWTAuthorizationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil,
+			UserDetailsService userDetailsService) {
+		super(authenticationManager);
+		this.jwtUtil = jwtUtil;
+		this.userDetailsService = userDetailsService;
+
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		String header = request.getHeader("Authorization"); /* Captura o valor que esta definido no cabeçalho da req */
+
+		/* P/ autorizar usuario */
+		if (header != null && header.startsWith("Bearer ")) { /* Se header começa com "Bearer " */
+			UsernamePasswordAuthenticationToken auth = getAuthentication(request, header.substring(7)); /* */
+
+			if (auth != null) {
+				SecurityContextHolder.getContext().setAuthentication(auth);
+			}
+
+		}
+		
+		chain.doFilter(request, response); /* Continua o fluxo de autenticação */
+
+	}
+
+	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request, String substring) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
